a16f89ef776c27f63425f16238b1a61adf7caed0
Bryleen <bryleen1@live.co.uk>
2022-04-10 22:57:36 +0000

    Updated file

--- application/models.py
+++ application/models.py
@@ -4,3 +4,9 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
